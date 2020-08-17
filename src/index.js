acf5c6b508fc281fb8c4be0559849583a374d20f
michael thomas <michael.thomas2014@gmail.com>
2020-08-17 19:50:03 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('#create-task-form').addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let node = document.createElement("Li");
+    let task = document.getElementById('new-task-description').value;
+    let new_node = document.createTextNode(task);
+    node.appendChild(new_node);
+    let taskList = document.getElementById('tasks');
+    taskList.appendChild(node);
+    
+  });
+  
+  
+  
 });
