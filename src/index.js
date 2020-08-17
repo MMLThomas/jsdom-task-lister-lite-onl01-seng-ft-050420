f44877dc2eb0bcc48fa544202df72652a3f1190e
michael thomas <michael.thomas2014@gmail.com>
2020-08-17 19:21:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector('#create-task-form').addEventListener("submit", function(event) {
     event.preventDefault();
+    let taskList = document.
   });
   
   
