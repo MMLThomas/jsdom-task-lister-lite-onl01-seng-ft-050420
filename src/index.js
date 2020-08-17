8266093dfbf0f8d94a6815c147013a3dd628878e
michael thomas <michael.thomas2014@gmail.com>
2020-08-17 19:12:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.querySelector('form').addEventListener("submit", function(event)){
+  document.getElementByquery('form').addEventListener("submit", function(event)){
     event.preventDefault();
   }
   
