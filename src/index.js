4426ad2f843b26113f5b66e963dfe4eafb5c060c
michael thomas <michael.thomas2014@gmail.com>
2020-08-17 19:46:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
     let node = document.createElement("Li");
     let task = document.getElementById('new-task-description').value;
-    let new_node = document.createTextNode("hello");
+    let new_node = document.createTextNode(task);
     node.appendChild(new_node);
     let taskList = document.getElementById('tasks');
     taskList.appendChild(node);
